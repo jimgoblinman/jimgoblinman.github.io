83e2dad8c012c524891434c3bc1a7bd355817db1
James Vining <jvi135606@stud.gibb.ch>
2023-08-07 14:17:01 +0200

    Github secrets

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
 const nextConfig = {
-    distDir: "build", // Set the custom build output directory
-    EMAIL_JS_USER_ID: process.env.NEXT_PUBLIC_EMAILJS_USER_ID,
-    EMAIL_JS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
-    EMAIL_JS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE
-    
-  };
-  
-  module.exports = nextConfig;
-  
+  distDir: "build", // Set the custom build output directory
+  env: {
+    NEXT_PUBLIC_EMAILJS_USER_ID: process.env.NEXT_PUBLIC_EMAILJS_USER_ID,
+    NEXT_PUBLIC_EMAILJS_SERVICE_ID: process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID,
+    NEXT_PUBLIC_EMAILJS_TEMPLATE_ID: process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID,
+  },
+};
+
+module.exports = nextConfig;
