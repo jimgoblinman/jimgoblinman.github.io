e0dd378db2ac84f291f957dfc72655b2454636a4
James Vining <jvi135606@stud.gibb.ch>
2023-08-26 11:46:30 +0200

    Changed directory of build folder

---
+++ build/server/chunks/411.js
@@ -0,0 +1,237 @@
+exports.id = 411;
+exports.ids = [411];
+exports.modules = {
+
+/***/ 1003:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"Footer-header": "Footer_Footer-header__IfLmS",
+	"social-container": "Footer_social-container__93b_i",
+	"name-container": "Footer_name-container__CbTh5",
+	"Footer-content": "Footer_Footer-content__MUQuV",
+	"contact": "Footer_contact__oICuX",
+	"info": "Footer_info__5aUEE",
+	"footer-mobile": "Footer_footer-mobile__Z5Pck",
+	"Footerbox": "Footer_Footerbox__Cn6DP"
+};
+
+
+/***/ }),
+
+/***/ 4666:
+/***/ ((module) => {
+
+// Exports
+module.exports = {
+	"header": "Header_header__072v2",
+	"hidden": "Header_hidden__KodJF",
+	"griditem": "Header_griditem__FRi7H",
+	"imageContainer": "Header_imageContainer__7N44E",
+	"roundImage": "Header_roundImage__1SZV_",
+	"mobile": "Header_mobile__V_nRy",
+	"mobile-menu": "Header_mobile-menu__GUaPa",
+	"mobile-menu-item": "Header_mobile-menu-item__VH7Ls",
+	"x-container": "Header_x-container__sMKHW",
+	"icon-container": "Header_icon-container__1rlmt"
+};
+
+
+/***/ }),
+
+/***/ 765:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+/* harmony export */ __webpack_require__.d(__webpack_exports__, {
+/* harmony export */   Z: () => (__WEBPACK_DEFAULT_EXPORT__)
+/* harmony export */ });
+/* harmony import */ var react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(5893);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(6689);
+/* harmony import */ var react__WEBPACK_IMPORTED_MODULE_1___default = /*#__PURE__*/__webpack_require__.n(react__WEBPACK_IMPORTED_MODULE_1__);
+/* harmony import */ var _styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(1003);
+/* harmony import */ var _styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default = /*#__PURE__*/__webpack_require__.n(_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2__);
+
+
+
+function Footer() {
+    return /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)(react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.Fragment, {
+        children: [
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default().Footerbox),
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["Footer-header"]),
+                        children: [
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["name-container"]),
+                                children: [
+                                    " ",
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "James Vining"
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("div", {}),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["social-container"]),
+                                children: [
+                                    " ",
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "https://wa.me/41797280659",
+                                        target: "_blank",
+                                        alt: "Whatsapp",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                            src: "assets/whatsapp.png"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "https://github.com/jimgoblinman",
+                                        target: "_blank",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                            src: "assets/github.png",
+                                            alt: "Github"
+                                        })
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                        href: "https://www.linkedin.com/in/jim-vin-421b42228/",
+                                        target: "_blank",
+                                        alt: "Linkedin",
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                            src: "assets/linkedin.png"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["Footer-content"]),
+                        children: [
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default().contact),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                                        children: "Contact"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "Phone number: +41 79 728 06 59"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "Email: jim.vining@outlook.com"
+                                    })
+                                ]
+                            }),
+                            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default().info),
+                                children: [
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                                        children: "Info"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: "\xa9 All rights reserved"
+                                    }),
+                                    /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                        children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                            href: "/impressum",
+                                            children: "Impressum"
+                                        })
+                                    })
+                                ]
+                            })
+                        ]
+                    })
+                ]
+            }),
+            /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["footer-mobile"]),
+                children: [
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["name-container"]),
+                        children: [
+                            " ",
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                children: "James Vining"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default()["social-container"]),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "https://wa.me/41797280659",
+                                target: "_blank",
+                                alt: "Whatsapp",
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                    src: "assets/whatsapp.png"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "https://github.com/jimgoblinman",
+                                target: "_blank",
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                    src: "assets/github.png",
+                                    alt: "Github"
+                                })
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                href: "https://www.linkedin.com/in/jim-vin-421b42228/",
+                                target: "_blank",
+                                alt: "Linkedin",
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("img", {
+                                    src: "assets/linkedin.png"
+                                })
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default().contact),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                                children: "Contact"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                children: "Phone number: +41 79 728 06 59"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                children: "Email: jim.vining@outlook.com"
+                            })
+                        ]
+                    }),
+                    /*#__PURE__*/ (0,react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxs)("div", {
+                        className: (_styles_Footer_module_css__WEBPACK_IMPORTED_MODULE_2___default().info),
+                        children: [
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("h2", {
+                                children: "Info"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                children: "\xa9 All rights reserved"
+                            }),
+                            /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("p", {
+                                children: /*#__PURE__*/ react_jsx_runtime__WEBPACK_IMPORTED_MODULE_0__.jsx("a", {
+                                    href: "/impressum",
+                                    children: "Impressum"
+                                })
+                            })
+                        ]
+                    })
+                ]
+            })
+        ]
+    });
+}
+/* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (Footer);
+
+
+/***/ }),
+
+/***/ 7311:
+/***/ (() => {
+
+
+
+/***/ })
+
+};
+;
